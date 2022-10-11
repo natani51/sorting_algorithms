e0a6e915f11c3c7df1938f6601df88569ba24e26
Natnael Endale <62070546+natani51@users.noreply.github.com>
2022-10-11 05:03:13 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,25 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
- */
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j, n;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	{
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j - 1] > array[j])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				n = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = n;
+				print_array(array, size);
 			}
 		}
+	}
 }
